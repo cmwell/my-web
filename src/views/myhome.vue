e2b1b03099bfcd11d869f0143744a5a52959c415
zhanghao <wellzhang@aliyun.com>
2019-09-27 19:23:04 +0800

    my vue

---
+++ src/views/myhome.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="content" >
+  <h1><b style="color: white">well_zh管理中心</b></h1>
+  <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+    <el-form-item label="账号" style="color: #fbfffd" >
+      <el-input v-model="ruleForm.pass" autocomplete="off"></el-input>
+    </el-form-item>
+    <el-form-item label="密码" style="color: black">
+      <el-input  v-model="ruleForm.checkPass" autocomplete="off"></el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-button  type="primary" @click="submitForm('ruleForm')">登录</el-button>
+    </el-form-item>
+  </el-form>
+</div>
+</template>
+<style>
+  @import "myhome.css";
+</style>
+<script>
+import myhome from './myhome.js'
+export default myhome
+</script>
